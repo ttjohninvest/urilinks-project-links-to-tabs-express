02485b69b4de887b76b18210404d22a7efa8b024
ttjohninvest <ttjohninvest@gmail.com>
2025-08-05 03:44:41 -0700

    port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 //urilinks-project-urls-to-tabs-html.vercel.app
@@ -82,6 +83,6 @@ a(req.body.url)
 
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 3000');
 });
